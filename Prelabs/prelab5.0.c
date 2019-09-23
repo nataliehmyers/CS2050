e8108bd0ab962a77521e504b72bb90e44d8b4264
Natalie Myers <nataliehmyers@gmail.com>
2019-09-22 21:03:55 -0500

    Completed Prelab5

--- Prelabs/prelab5.0.c
+++ Prelabs/prelab5.0.c
@@ -10,11 +10,23 @@ int readRecordFile(char *filename, Record ***array);
 void adjustSalaries(Record **employees, int position, float x);
 void freeRecordArray(Record ***array, int numElems);
 
+// This needs to be global; see https://stackoverflow.com/a/10349610
+int size;
+
 int main(void){
     Record ***array = malloc(sizeof(int));
     char filename[100] = "/Users/natalie/Documents/CLionProjects/CS2050/Prelabs/employee.csv";
-    int size = readRecordFile(filename, array);
-    printf("Emp ID: %d SSN: %d", (*array)[0]->empID, (*array)[0]->ssn);
+    size = readRecordFile(filename, array);
+//    printf("Emp ID: %d SSN: %d", (*array)[1]->empID, (*array)[1]->ssn);
+    int position;
+    float x;
+//    printf("Enter position number and raise modifier: ");
+//    scanf("%d %f", &position, &x);
+    position, x = 0;
+    adjustSalaries(*array, position, x);
+    freeRecordArray(array, size);
+    array = NULL;
+    return 0;
 }
 
 int readRecordFile(char *filename, Record ***array){
@@ -40,4 +52,21 @@ int readRecordFile(char *filename, Record ***array){
     *array = temp_array;
     fclose(fp);
     return size;
+}
+
+// missing size parameter, https://stackoverflow.com/a/10349610
+void adjustSalaries(Record **employees, int position, float x){
+    for(int i = 0; i < size; i++){
+        if(employees[i]->empID == position){
+            employees[i]->salary = employees[i]->salary * x;
+        }
+    }
+}
+
+void freeRecordArray(Record ***array, int numElems){
+    for(int i = 0; i < numElems; i++) {
+        free((*array)[i]);
+    }
+    free(*array);
+    free(array);
 }
