fbd2bab7505105925539f8ad5a47c1e5de863b47
Natalie Myers <nataliehmyers@gmail.com>
2019-10-29 15:39:22 -0500

    Created lab11.c

---
+++ In-ClassLabs/New_Labs/lab11.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct nodestruct {
+    int key;
+    float priority;
+    struct nodestruct *next;
+} Node;
+typedef struct {
+    int size;
+    Node *front, *rear;
+} PQueue;
+
+PQueue* initPQ();
+int insertPQ(int, float, PQueue*);
+int deleteMinPQ(PQueue*, int*);
+void reInitPQ(PQueue*);
+void freePQ(PQueue*);
+void printPQ(PQueue*);
+
+int main(void){
+    PQueue* queue = initPQ();
+    insertPQ(1, 99, queue);
+    printPQ(queue);
+    insertPQ(2, 0, queue);
+    printPQ(queue);
+    insertPQ(3, 50, queue);
+    printPQ(queue);
+
+    int removedItem = 0;
+    int* key = &removedItem;
+    deleteMinPQ(queue, key);
+    printPQ(queue);
+    deleteMinPQ(queue, key);
+    printPQ(queue);
+    deleteMinPQ(queue, key);
+    printPQ(queue);
+
+    return 0;
+}
+
+PQueue* initPQ(){
+    PQueue* queue = malloc(sizeof(PQueue));
+    if(queue == NULL){
+        return NULL;
+    }
+    queue->front = NULL;
+    queue->rear = NULL;
+    queue->size = 0;
+    return queue;
+}
+
+int insertPQ(int key, float priority, PQueue* queue){
+    Node* newNode = malloc(sizeof(Node));
+    if(newNode == NULL){
+        return -1;
+    }
+    newNode->key = key;
+    newNode->priority = priority;
+    queue->size++;
+    if(queue->rear == NULL){
+        queue->rear = newNode;
+        newNode->next = newNode;
+    } else if (priority > queue->rear->priority) {
+        newNode->next = queue->rear->next;
+        queue->rear->next = newNode;
+        queue->rear = newNode;
+    } else {
+        Node* currentNode = queue->rear;
+        while (priority > currentNode->next->priority) {
+            currentNode = currentNode->next;
+        }
+        newNode->next = currentNode->next;
+        currentNode->next = newNode;
+    }
+    return 0;
+}
+
+int deleteMinPQ(PQueue* queue, int* key){
+    if(queue->rear == NULL){
+        return -1;
+    }
+    if(key == NULL){
+        return -2;
+    }
+    if(queue->size == 1){
+        *key = queue->rear->next->key;
+        free(queue->rear->next);
+        queue->size--;
+        queue->rear = NULL;
+        return 0;
+    }
+    Node *removeNode = queue->rear->next;
+    *key = removeNode->key;
+    queue->rear->next = removeNode->next;
+    queue->size--;
+    free(removeNode);
+    return 0;
+}
+
+void reInitPQ(PQueue* queue){
+    while(queue->size > 0){
+        int poppedValue = 0;
+        deleteMinPQ(queue, &poppedValue);
+    }
+}
+
+void printPQ(PQueue* queue){
+    if (queue == NULL){
+        return;
+    }
+    if(queue->rear == NULL){
+        return;
+    }
+    Node* currentPtr = queue->rear->next;
+    while(currentPtr->next != queue->rear->next){
+        printf("Key: %d\tPriority: %.2f\n", currentPtr->key, currentPtr->priority);
+        currentPtr = currentPtr->next;
+    }
+    printf("Key: %d\tPriority: %.2f", currentPtr->key, currentPtr->priority);
+    printf("\n");
+    printf("----------------------------------\n");
+}
+
+void freePQ(PQueue* queue){
+    reInitPQ(queue);
+    free(queue);
+    queue = NULL;
+}
